72c0e78cb9e0cd5732bb53e06034eac4f3cc3ca6
sudondie <ilia.skakov@gmail.com>
2020-12-26 14:08:24 +0300

    replaced callbacks by promise

--- Node.js course/node-mongo/index.js
+++ Node.js course/node-mongo/index.js
@@ -5,31 +5,32 @@ const dboper = require("./operations");
 const url = "mongodb://localhost:27017/";
 const dbname = "conFusion";
 
-mongo.connect(url, (err, client) => {
-  assert.strictEqual(err, null);
-  console.log("Подключился к серверу");
+mongo.connect(url).then((client) => {
+  console.log('Connected correctly to server');
   const db = client.db(dbname);
-  dboper.insertDocument(db,{ name: "Vadonut", description: "Test" },"dishes",(result) => {
-      console.log("Вставил в документ:\n", result.ops);
-      dboper.findDocuments(db, "dishes", (docs) => {
-        console.log("Найдено:\n", docs);
-        dboper.updateDocument(
-          db,
-          { name: "Vadonut" },
-          { description: "Updated Test" },
-          "dishes",
-          (result) => {
-            console.log("Обновил документ:\n", result.result);
-            dboper.findDocuments(db, "dishes", (docs) => {
-              console.log("Найдено обновленны документов:\n", docs);
-              db.dropCollection("dishes", (result) => {
-                console.log("Dropped Collection: ", result);
-                client.close();
-              });
-            });
-          }
-        );
-      });
-    }
-  );
-});
+  dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+      "dishes")
+      .then((result) => {
+          console.log("Insert Document:\n", result.ops);
+          return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
+          console.log("Found Documents:\n", docs);
+          return dboper.updateDocument(db, { name: "Vadonut" },
+                  { description: "Updated Test" }, "dishes");
+      })
+      .then((result) => {
+          console.log("Updated Document:\n", result.result);
+          return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
+          console.log("Found Updated Documents:\n", docs);           
+          return db.dropCollection("dishes");
+      })
+      .then((result) => {
+          console.log("Dropped Collection: ", result);
+          return client.close();
+      })
+      .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
--- Node.js course/node-mongo/operations.js
+++ Node.js course/node-mongo/operations.js
@@ -1,37 +1,20 @@
-const assert = require("assert");
-
-exports.insertDocument = (db,document,collection,callback) => {
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err,result) => {
-        assert.strictEqual(err,null);
-        console.log("inserted "+ result.result.n + ' document into the collection '+ collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
-exports.findDocuments = (db,collection,callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err,docs)=> {
-        assert.strictEqual(err,null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
-exports.removeDocument = (db,document,collection,callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err,result) => {
-        assert.strictEqual(err,null);
-        console.log("Remove the document ", document);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db,document,update,collection,callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection);
-    coll.updateOne(document,{$set: update}, null, (err,result) => {
-        assert.strictEqual(err,null);
-        console.log("Updated the document with ", update);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
 
