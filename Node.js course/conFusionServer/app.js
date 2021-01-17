12353ceaabca0f5e349a71031f5a1bc1d7cc3e78
sudondie <ilia.skakov@gmail.com>
2021-01-17 15:52:36 +0300

    IDK

--- Node.js course/conFusionServer/app.js
+++ Node.js course/conFusionServer/app.js
@@ -4,6 +4,8 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let session = require("express-session");
+let FileStore = require('session-file-store')(session);
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
@@ -33,27 +35,45 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
-app.use(cookieParser());
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  let authHeader = req.headers.authorization;
-  if (!authHeader) {
-    let err = new Error("you are not logged in!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
-  }
-  let auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  let username = auth[0];
-  let password = auth[1];
-  if (username == 'admin' && password == 'password') {
-    next();
+  console.log(req.session);
+  if (!req.session.user) {
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+      let err = new Error("you are not logged in!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+    let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    let username = auth[0];
+    let password = auth[1];
+    if (username == 'admin' && password == 'password') {
+      req.session.user = 'admin';
+      next();
+    } else {
+      let err = new Error("you are not logged in!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    let err = new Error("you are not logged in!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
+    if (req.session.user === 'admin') {
+      console.log('req.session ' + req.session);
+      next();
+    } else {
+      let err = new Error("you are not logged in!");
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 app.use(auth);
--- Node.js course/conFusionServer/routes/users.js
+++ Node.js course/conFusionServer/routes/users.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let bodyParser = require('body-parser');
+let user = require('../models/user');
+let router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
