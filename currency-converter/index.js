6aad3c2d3ca2cb5996cd1c9c789bd9a14be159af
sudondie <ilia.skakov@gmail.com>
2021-02-17 20:35:20 +0300

    added some styles and currency API

--- currency-converter/index.js
+++ currency-converter/index.js
@@ -1,16 +1,39 @@
-async function something(question) {
+const axios = require('axios');
+const getExchangeRate = async (fromCurrency, toCurrency) => {
     try {
-        let promise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(question);
-            }, 1000);
-        });
-        let result = await promise;
-        let check = prompt(result);
-        (check !== '') ? alert(check): alert(new Error('Вы ввели пустую строку'));
+        const response = await axios.get('http://api.currencylayer.com/live?access_key=5e6a36bc33c0f6d68f1ee378738388ef');
+        const rate = response.data.quotes; //объект с курсами валют
+        const baseCurrency = response.data.source; //базовая валюта
+        const usd = 1 / rate[`${baseCurrency}${fromCurrency}`]; //
+        const exchangeRate = usd * rate[`${baseCurrency}${toCurrency}`];
+        return exchangeRate;
     } catch (err) {
-        throw new Error(err);
+        throw new Error(`Не получается перевести ${fromCurrency} to ${toCurrency}`);
+    }
+};
+
+const getCurrencyCountries = async (currencyCode) => {
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+        return response.data.map(country => country.name);
+    } catch (err) {
+        throw new Error(`Не получается найти страну(ы) где используют ${currencyCode}`);
     }
 }
-something('Как вам?');
-console.log('wow');
+
+const currencyConvert = async (fromCurrency, toCurrency, amount) => {
+    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+    const countries = await getCurrencyCountries(toCurrency);
+    const converted = (amount * exchangeRate).toFixed(3);
+    return `${amount} ${fromCurrency} стоит ${converted} ${toCurrency}. Их можно потратить в: \n ${countries}`;
+}
+
+function fuck() {
+    const from = document.getElementById("from").value;
+    const to = document.getElementById("to").value;
+    const amount = document.getElementById("amount").value;
+    currencyConvert(from.toUpperCase(), to.toUpperCase(), Number(amount))
+        .then(message => alert(message))
+        .catch(err => console.log(err.message));
+}
+document.getElementById('submit').onclick = fuck;
--- just learning/learn2.js
+++ just learning/learn2.js
@@ -313,3 +313,20 @@ class Citizen extends Human {
 }
 const Ilia = new Citizen('Ilia', 21, new Date(Date.now()), 'Rostov-on-Don')
 console.log(Ilia)
+
+async function something(question) {
+  try {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(question);
+      }, 1000);
+    });
+    let result = await promise;
+    let check = prompt(result);
+    (check !== '') ? alert(check): alert(new Error('Вы ввели пустую строку'));
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+something('Как вам?');
+console.log('wow');
